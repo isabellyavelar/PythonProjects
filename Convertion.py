d1c9eb06d4ca86c580e533ce8ffd89dffee28cee
Royd Catalunes <156393347+Royd0101@users.noreply.github.com>
2024-11-11 15:28:19 +0800

    Update Convertion.py

--- Convertion.py
+++ Convertion.py
@@ -1,1 +1,13 @@
-# Amp-hours to Wats Convertion Basic Code
+# Amp-hours to Watts Conversion Basic Code
+
+#Enter thenumber of Amp-hours
+amp_hour = int(input("Enter the Amp-hours: "))
+
+#Enter thenumber of volts
+volts = int(input("Enter the voltage: "))
+
+# multiply the amp_hour to volts
+Wh = amp_hour * volts
+
+#print the output
+print("Watts is:", Wh)
